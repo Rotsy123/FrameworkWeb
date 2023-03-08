4deef93d12e4e2852ff3890a4c58197132d8368b
Rotsy123 <rakotondrainyrotsy@gmail.com>
2023-03-08 19:48:19 +0300

    Sprint2

---
+++ test/WEB-INF/script.bat
@@ -0,0 +1,9 @@
+cd etu2009.framework.servlet/servlet
+javac -d . GetUrl.java
+move etu2009 ../model
+cd ../model
+javac -d . *.java
+move etu2009 ../servlet
+cd ../servlet
+javac -d . *.java
+move etu2009 ../../classes
