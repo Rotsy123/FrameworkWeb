a2fef6d7da0313960b7bb2fc7d1f935aa83ee34d
Rotsy123 <rakotondrainyrotsy@gmail.com>
2023-07-07 22:26:37 +0300

    sprint14

--- test/framework/WEB-INF/etu2009.framework.servlet/servlet/GetUrl.java
+++
@@ -1,8 +0,0 @@
-package etu2009.framework.servlet;
- 
-import java.lang.annotation.*;
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface GetUrl {
-    String url() default "";
-}
